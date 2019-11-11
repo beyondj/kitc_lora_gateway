2f494754bd70d6f07fb7aaaffde93ef5c4b68174
Kim <beyondj.kim@gmail.com>
2019-11-11 13:59:29 +0000

    cpu freq and number of cpu

--- power/all_enable.sh
+++ power/all_enable.sh
@@ -1,5 +1,13 @@
-sudo sed -i '/dtparam=audio=off/d' /boot/config.txt
-sudo sed -i '/dtoverlay=pi3-disable-bt/d' /boot/config.txt
+# cpu
+sudo sed -i 's/maxcpus=2 //' /boot/cmdline.txt
+sudo sed -i '/arm_freq=700/d' /boot/config.txt
 
+# audio
+sudo sed -i '/dtparam=audio=off/d' /boot/config.txt
+
+
+# bluetooth
+sudo sed -i '/dtoverlay=pi3-disable-bt/d' /boot/config.txt
 sudo systemctl enable hciuart
+
 sudo reboot
--- power/static_disable.sh
+++ power/static_disable.sh
@@ -1,3 +1,5 @@
+sudo sed -i '1s/^/maxcpus=2 /' /boot/cmdline.txt
+
 sudo cat disable.txt >> /boot/config.txt
 
 sudo systemctl disable hciuart
